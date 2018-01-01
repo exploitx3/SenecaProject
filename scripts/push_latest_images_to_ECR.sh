3746ab26dbe9dabec98158043e3a778181c07f4d
exploitx3 <gogo333x@gmail.com>
2018-01-01 23:16:25 +0000

    add script to push images to ECR

---
+++ scripts/push_latest_images_to_ECR.sh
@@ -0,0 +1,16 @@
+docker tag user-service:latest 453247265801.dkr.ecr.eu-west-2.amazonaws.com/user-service:latest
+docker push 453247265801.dkr.ecr.eu-west-2.amazonaws.com/user-service:latest
+
+docker tag web-service:latest 453247265801.dkr.ecr.eu-west-2.amazonaws.com/web-service:latest
+docker push 453247265801.dkr.ecr.eu-west-2.amazonaws.com/web-service:latest
+
+docker tag math-service:latest 453247265801.dkr.ecr.eu-west-2.amazonaws.com/math-service:latest
+docker push 453247265801.dkr.ecr.eu-west-2.amazonaws.com/math-service:latest
+
+docker tag ping-service:latest 453247265801.dkr.ecr.eu-west-2.amazonaws.com/ping-service:latest
+docker push 453247265801.dkr.ecr.eu-west-2.amazonaws.com/ping-service:latest
+
+
+
+
+
